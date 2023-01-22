7c0db9f174fc689d0738ab8d982efeab6af970c3
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2023-01-22 11:15:50 -0300

    initial commit

---
+++ Problems.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Problems.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Problems/Class1.cs
@@ -0,0 +1,7 @@
+﻿// DESCRIPTION: https://leetcode.com/problems/knight-probability-in-chessboard/
+
+namespace Problems;
+
+public class Class1
+{
+}
