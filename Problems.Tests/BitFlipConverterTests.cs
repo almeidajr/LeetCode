bd98428f45bca27e420b37faf27fa17b9ffd5412
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2023-01-28 14:26:35 -0300

    feat(problem): 2220. Minimum Bit Flips to Convert Number

---
+++ Problems.Tests/BitFlipConverterTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Problems.Tests;
+
+public class BitFlipConverterTests
+{
+    [Theory]
+    [InlineData(0b1010, 0b0111, 3)]
+    [InlineData(0b0011, 0b0100, 3)]
+    [InlineData(0b0100, 0b0101, 1)]
+    [InlineData(0b0111, 0b0111, 0)]
+    [InlineData(0b1100, 0b1111, 2)]
+    public void MinBitFlips_WhenCalled_ShouldReturnCorrectNumberOfFlips(int start, int goal, int expected)
+    {
+        // Act
+        var actual = BitFlipConverter.MinBitFlips(start, goal);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+}
---
+++ Problems/BitFlipConverter.cs
@@ -0,0 +1,12 @@
+﻿// DESCRIPTION: https://leetcode.com/problems/minimum-bit-flips-to-convert-number/
+
+namespace Problems;
+
+public static class BitFlipConverter
+{
+    public static int MinBitFlips(int start, int goal)
+    {
+        var flip = start ^ goal;
+        return Convert.ToString(flip, 2).Count(c => c == '1');
+    }
+}
