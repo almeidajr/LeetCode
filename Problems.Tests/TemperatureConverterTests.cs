10ba5a919915e28ad1603dd7c9a21be7928c9068
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2023-01-28 13:42:09 -0300

    feat(problem): 2469. Convert the Temperature

---
+++ Problems.Tests/TemperatureConverterTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems.Tests;
+
+public class TemperatureConverterTests
+{
+    [Fact]
+    public void ConvertTemperature_WhenCalled_ShouldReturnArrayLength2()
+    {
+        // Act
+        var actual = TemperatureConverter.ConvertTemperature(0);
+
+        // Assert
+        actual.Length.Should().Be(2);
+    }
+
+    [Theory]
+    [InlineData(36.50, new[] { 309.65, 97.7 })]
+    [InlineData(122.11, new[] { 395.26, 251.798 })]
+    public void ConvertTemperature_WithCelsiusTemperature_ShouldReturnFahrenheitAndKelvin(
+        double celsius,
+        double[] expected)
+    {
+        // Act
+        var actual = TemperatureConverter.ConvertTemperature(celsius);
+
+        // Assert
+        actual[0].Should().BeApproximately(expected[0], 1e-5);
+        actual[1].Should().BeApproximately(expected[1], 1e-5);
+    }
+}
---
+++ Problems/TemperatureConverter.cs
@@ -0,0 +1,21 @@
+﻿// DESCRIPTION: https://leetcode.com/problems/convert-the-temperature/
+
+namespace Problems;
+
+public static class TemperatureConverter
+{
+    public static double[] ConvertTemperature(double celsius)
+    {
+        return new[] { CelsiusToKelvin(celsius), CelsiusToFahrenheit(celsius) };
+    }
+
+    private static double CelsiusToKelvin(double celsius)
+    {
+        return celsius + 273.15;
+    }
+
+    private static double CelsiusToFahrenheit(double celsius)
+    {
+        return celsius * 1.8 + 32;
+    }
+}
